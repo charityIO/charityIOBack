042faff03b45d4f8bedd90e819502c55948ced51
usmanabdurrehman <saidskit@outlook.com>
2021-06-29 21:17:42 +0500

    Changed PORT + added Procfile

--- index.js
+++ index.js
@@ -38,6 +38,6 @@ app.use('/',require('./api/generalapi'))
 app.use('/user',require('./api/userapi'))
 
 
-app.listen(7000,()=>{
+app.listen(process.env.PORT || 7000,()=>{
 	console.log('Listening on port 7000')
 })
